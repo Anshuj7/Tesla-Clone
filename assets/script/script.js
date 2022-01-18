e6f2a7f6f321e8e32ab0d0950363f028ebe62964
Anshuj7 <48627609+Anshuj7@users.noreply.github.com>
2022-01-18 17:49:59 +0530

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,22 @@
+const overlay = document.querySelector('.overlay');
+const overlayClose = document.querySelector('.overlay__close');
+
+const openMenu = document.querySelectorAll('.open-menu');
+openMenu.forEach(menuBtn => {
+    menuBtn.addEventListener('click', (e) => {
+        overlay.classList.add('show')
+    });
+});
+overlayClose.addEventListener('click', (e) => {
+    overlay.classList.remove('show')
+});
+
+ScrollOut({
+    scrollingElement: ".container",
+    onShown: function(el){
+        el.animate([{ opacity: 0 }, { opacity: 1 }], 1500);
+    },
+    onHidden: function(el){
+        el.animate([{ opacity: 1 }, { opacity: 0 }], 1500);
+    }
+});
